e914a1d7fb46e09fe1a35a921d3b1a122ee2718f
aaron_tou <tou791230@gmail.com>
2021-09-27 09:16:58 +0800

    init

---
+++ TestGraceful/Program.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Runtime.Loader;
+
+namespace TestGraceful
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			AssemblyLoadContext.Default.Unloading += ctx =>
+			{
+				System.Console.WriteLine("--------------------");
+			};
+
+			Console.WriteLine("Hello World!");
+			var host = CreateHostBuilder(args).Build();
+			host.Run();
+		}
+		private static IHostBuilder CreateHostBuilder(string[] args)
+		{
+			return Host.CreateDefaultBuilder()
+				.ConfigureAppConfiguration((hostContext, configbuilder) =>
+				{
+					var ienv = hostContext.HostingEnvironment;
+					configbuilder.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+						  .AddJsonFile($"appsettings.{ienv.EnvironmentName}.json", optional: true, reloadOnChange: true)
+						  .AddEnvironmentVariables()
+						  .Build();
+
+				}).ConfigureServices((hostContext, services) =>
+				{
+					services.Configure<IConfiguration>(hostContext.Configuration);
+					services.AddLogging();
+					services.AddHostedService<ShutdownService>();
+
+					services.Configure<HostOptions>(opts => opts.ShutdownTimeout = TimeSpan.FromSeconds(15));
+				}).UseConsoleLifetime();
+		}
+	}
+}
---
+++ TestGraceful/ShutdownService.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.Extensions.Hosting;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Loader;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TestGraceful
+{
+	public class ShutdownService : IHostedService
+    {
+        private bool pleaseStop;
+        private Task BackgroundTask;
+        private readonly IHostApplicationLifetime applicationLifetime;
+
+        public ShutdownService(IHostApplicationLifetime applicationLifetime)
+        {
+            this.applicationLifetime = applicationLifetime;
+        }
+
+        public Task StartAsync(CancellationToken _)
+        {
+
+			Log.Logger = new LoggerConfiguration()
+				.WriteTo.Seq("http://172.23.80.1:5300")
+				.CreateLogger();
+            Log.Information("Test!!");
+
+            AssemblyLoadContext.Default.Unloading += ctx =>
+            {
+                System.Console.WriteLine("XXXXXXXXXXXXXXXXXXX");
+                Log.Information("XXXXXXXXXXXXXXXXXXX");
+            };
+
+            Console.WriteLine("Starting service:" + DateTime.Now);
+
+            this.applicationLifetime.ApplicationStarted.Register(() =>
+            {
+                Console.WriteLine("ApplicationStarted");
+            });
+
+            this.applicationLifetime.ApplicationStopping.Register(() =>
+            {
+                Console.WriteLine("ApplicationStopping");
+
+            });
+            this.applicationLifetime.ApplicationStopped.Register(() =>
+            {
+
+                Console.WriteLine("ApplicationStopped SIGTERM received");
+                Log.Information("ApplicationStopped SIGTERM received");
+                Thread.Sleep(3_000);
+                Log.Information("Termination delay complete, continuing stopping process");
+                Console.WriteLine("Termination delay complete, continuing stopping process");
+                Environment.Exit(0);
+            });
+
+			BackgroundTask = Task.Run(async () =>
+			{
+                int a = 0;
+				while (!pleaseStop)
+				{
+                    Console.WriteLine(a++);
+                    Log.Information(a.ToString());
+                    await Task.Delay(5000);
+				}
+
+				Console.WriteLine("Background task gracefully stopped");
+			});
+
+			return Task.CompletedTask;
+        }
+
+        public async Task StopAsync(CancellationToken cancellationToken)
+        {
+            Console.WriteLine("Stopping service");
+
+            pleaseStop = true;
+            await BackgroundTask;
+
+            Console.WriteLine("Service stopped");
+        }
+    }
+}
